2cae7f784f3d647b1be79497fb0e13eb813caab0
Mary Dickson <mdickson@gmail.com>
2016-06-29 23:57:23 -0700

    code refactors and rubocop

--- solutions/picofermibagels.rb
+++ solutions/picofermibagels.rb
@@ -1,6 +1,5 @@
 # to run: $ ruby ./solutions/picofermibagels.rb
-
-class Referee
+class Referee 
   def initialize
     @num = pick_number
     @count = 0
@@ -14,45 +13,48 @@ class Referee
   def check
     @count += 1
     guess = Player.guess
+    return win(guess) if guess == @num
     reply = fermi_pico(guess)
-    reply += "Bagels" if reply.empty?
-    return win(guess) if reply == "Fermi Fermi Fermi Fermi"
+    reply += 'Bagels' if reply.empty?
     puts reply
-  end
-
-  def fermi_pico(guess)
-    4.times.map do |i|
-      if guess[i] == @num[i]
-        "Fermi"
-      elsif @num.index(guess[i])
-        "Pico"
-      end
-    end.compact.sort.join(" ")
   end
 
   private
 
+  def fermi_pico(guess)
+    Array.new(4) do |i|
+      if guess[i] == @num[i]
+        'Fermi'
+      elsif @num.index(guess[i])
+        'Pico'
+      end
+    end.compact.sort.join(' ')
+  end
+
   def pick_number
-    (0..9).to_a.shuffle[0..3].join
+    (0..9).to_a.sample(4).join
   end
 
   def win(guess)
-    puts "you guessed the correct number, #{guess}!! it only took #{@count} tries."
+    puts "you guessed the correct number, #{guess}!!"
+    puts "it only took #{@count} tries."
     @guessed = true
   end
 end
 
 class Player
   def self.guess
-    puts "guess a 4 digit number (no repeating numbers)"
-    guess = gets.chomp()
-    return guess unless (guess =~ /^[\d]{4}\b/).nil? || guess.split("").uniq.length != 4
-    puts "guess must be 4 unique digits. guess again!"
+    puts 'guess a 4 digit number (no repeating numbers)'
+    guess = gets.chomp
+    return guess unless invalid(guess)
+    puts 'guess must be 4 unique digits. guess again!'
     Player.guess
+  end
+
+  def self.invalid(guess)
+    (guess =~ /^[\d]{4}\b/).nil? || guess.split('').uniq.length != 4
   end
 end
 
 ref = Referee.new
-until ref.guessed?
-  ref.check
-end
+ref.check until ref.guessed?
